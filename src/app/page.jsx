67febfc8caf0cbfc0e518b3146fdf3b4153783b6
Sumedh Sangle <sanglesumedh15@gmail.com>
2026-01-07 15:34:30 +0530

    cta added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -45,7 +45,7 @@ export default function HomePage() {
         {/* <CollegeUnplugged /> */}
         {/* <EventsSection /> */}
         <NewCTA />
-        {/* <CTASection /> */}
+        <CTASection />
       </ScrollCrushWrapper>
 
       <InstagramMosaic />
